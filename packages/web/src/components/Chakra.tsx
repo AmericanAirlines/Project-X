4121dacaa94c217a6d4efa93001fbc7305d5e362
John Kahn <7807353+johnkahn@users.noreply.github.com>
2021-09-10 11:25:52 -0400

    Prettier

--- packages/web/src/components/Chakra.tsx
+++ packages/web/src/components/Chakra.tsx
@@ -17,9 +17,9 @@ export const Chakra: React.FC<ChakraProps> = ({ cookies, children }) => (
  * Sends the cookies to the page to remove the flash. This disables the static
  * page generation when exported from a page, so only use it on pages where you
  * think most users will go to the url directly (the homepage, etc)
- * 
+ *
  * Add it to a page like this
- * 
+ *
  * `export { getServerSideProps } from '../components/Chakra';`
  */
 export const getServerSideProps: GetServerSideProps = async ({
