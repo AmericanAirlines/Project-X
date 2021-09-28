28570a8e2dd2aa66f2f413f2e7c0113bcfa318e7
Tevin Beckwith <txb131330@utdallas.edu>
2021-09-28 04:42:18 -0500

    get database working

--- packages/api/src/entities/Video.ts
+++ packages/api/src/entities/Video.ts
@@ -1,3 +1,21 @@
+import { Entity, Property } from '@mikro-orm/core';
+import { ConstructorValues } from '../utils/types';
+import { Node } from './Node';
+
+export type VideoConstructorValues = ConstructorValues<Video>;
+
+@Entity()
+export class Video extends Node<Video> {
+  @Property({ columnType: 'text' })
+  name: string;
+
+  constructor({ name, ...extraValues }: VideoConstructorValues) {
+    super(extraValues);
+
+    this.name = name;
+  }
+}
+
 // new video entity
 /*
 
