ba73f80ef06f64d79c6ba4276adb75931bd6539d
Ayush Agarwal <ayush0316@gmail.com>
2021-09-28 20:02:07 -0500

    initial user profile api endpoint
    update index.ts with api route
    create user.ts with GET endpoint and use User entity

--- packages/api/src/api/index.ts
+++ packages/api/src/api/index.ts
@@ -1,7 +1,9 @@
 /* istanbul ignore file */
 import { Router } from 'express';
 import { health } from './health';
+//import {user} from './user'; 
 
 export const api = Router();
 
 api.use('/health', health);
+//api.use('/user'), user);
---
+++ packages/api/src/api/user.ts
@@ -0,0 +1,9 @@
+//import {User} from '../entities/User';
+//import { Router } from 'express';
+
+//export const user = Router();
+
+
+
+//add GET endpoint to return all user profile information from database
+//Parameter: id
