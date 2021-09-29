3971af6e1b88c9a49d8e091468ad8e63dff27338
Ayush Agarwal <ayush0316@gmail.com>
2021-09-28 20:02:45 -0500

    add fields to User entity

--- packages/api/src/entities/User.ts
+++ packages/api/src/entities/User.ts
@@ -9,10 +9,19 @@ export type UserConstructorValues = ConstructorValues<User>;
 export class User extends Node<User> {
   @Property({ columnType: 'text' })
   name: string;
+  //pronouns: string;
+  //location: string;
+  //hireable: boolean;
+  //purpose: string;
+  //schoolName:string;
+  //major: string;
+  //graduationDate: Date
+
 
   constructor({ name, ...extraValues }: UserConstructorValues) {
     super(extraValues);
 
     this.name = name;
+    //add other fields to constructor
   }
 }
