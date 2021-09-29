07e931a7b7c377a279d22b91756661b0a858ab6f
Tevin Beckwith <txb131330@utdallas.edu>
2021-09-29 13:57:52 -0500

    add get video by adi api endpoint

--- packages/api/src/api/videos.ts
+++ packages/api/src/api/videos.ts
@@ -1,17 +1,29 @@
 import {Router} from 'express';
+import { json } from 'stream/consumers';
 import { Video } from '../entities/Video';
 
 export const videos = Router();
 
-videos.get('', (req, res) => {
+videos.get('', async (req, res) => {
     try {
         const videosRepository = req.entityManager.getRepository(Video);
-        const videosList = videosRepository.findAll();
+        const videosList = await videosRepository.findAll();
 
         res.status(200).send(videosList);
     } catch (error) {
         res.status(500).send('There was an issue geting all videos');
     };
 });
-// add GET endpoint to return all videos at ''
-// add GET endpoint to return a video at '/{video_id}'
+
+videos.get('/:video_id', async (req, res) => {
+    const { video_id } = req.params;
+    
+    try {      
+        const videosRepository = req.entityManager.getRepository(Video);
+        const video = await videosRepository.findOne({ video_id: Number(video_id) });
+
+        res.status(200).send(video);
+    } catch (error) {
+        res.status(500).send('There was an issue geting the video with an id of ' + video_id);
+    };
+});
