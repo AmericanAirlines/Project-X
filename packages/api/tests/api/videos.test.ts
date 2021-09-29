2dba2fc2e7e4bd585b11a5997478770381b81b14
Tevin Beckwith <txb131330@utdallas.edu>
2021-09-29 17:21:58 -0500

    test work comment update

--- packages/api/tests/api/videos.test.ts
+++ packages/api/tests/api/videos.test.ts
@@ -2,14 +2,14 @@
 import supertest from 'supertest';
 import { videos } from '../../src/api/videos';
 import { testHandler } from '../testUtils/testHandler';
-import {app} from '../../src/index'
+
 
 // Need to mock the database for testing?
 
 describe('videos', () => {
   it("/videos returns all videos", async () => {
 
-    const { body } = await testHandler(videos).expect(200); //Actual API works, but this test fails?  Possible problem with how test is set up? Note: Testhandler appears to only call '/', check if it is calling all endpoints?
+    const { body } = await testHandler(videos).expect(200); //Actual API works, but this test fails?  Possible problem with how test is set up? Note to self: Testhandler appears to only call '/', check if it is calling all endpoints?
 
   });
 });
