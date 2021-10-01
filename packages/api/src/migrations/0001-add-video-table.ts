d6d624222f2750434501a203cfc91491c2e0dbbd
Tevin Beckwith <txb131330@utdallas.edu>
2021-10-01 03:46:52 -0500

    database column changes

--- packages/api/src/migrations/0001-add-video-table.ts
+++ packages/api/src/migrations/0001-add-video-table.ts
@@ -3,7 +3,7 @@ import { Migration } from '@mikro-orm/migrations';
 export class Migration20210928102009 extends Migration {
 
   async up(): Promise<void> {
-    this.addSql('create table "video" ("video_id" bigserial primary key, "title" text not null, "duration_in_seconds" int not null, "url" text not null);');
+    this.addSql('create table "video" ("id" bigserial primary key, "title" text not null, "durationInSeconds" int not null, "url" text not null);');
   }
 
   async down(): Promise<void> {
