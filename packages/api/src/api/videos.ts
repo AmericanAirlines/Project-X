04dc46bb00e0cd11310a624b0bd2d7e8d2e64053
Tevin Beckwith <txb131330@utdallas.edu>
2021-10-01 14:52:54 -0500

    PR updates, type checking, authors update

--- packages/api/src/api/videos.ts
+++ packages/api/src/api/videos.ts
@@ -21,13 +21,27 @@ videos.get('', async (req, res) => {
 videos.get('/:videoId', async (req, res) => {
     const { videoId } = req.params;
 
-    try {      
+    try {   
+        // Check if videoId is in the expected format
+        if (isNaN(+videoId)) {
+            throw new TypeError();
+        }  
+
+        // Find desired Video
         const videosRepository = req.entityManager.getRepository(Video);
         const video = await videosRepository.findOne({ id: videoId });
-
         res.status(200).send(video);
+        
     } catch (error) {
-        logger.error('There was an issue geting video "' + videoId + '":', error);
-        res.status(500).send('"' + videoId + '" is not a valid id');
+        // Check for error type
+        if (error instanceof TypeError) {
+            logger.error('videoId "' + videoId + '" is not in the expected format.', error);
+            // Return Unprocessable Enitity Status Code
+            res.status(422).send('"' + videoId + '" is not a valid id');
+        } else {
+            logger.error('There was an issue geting video "' + videoId + '":', error);
+            res.status(500).send('There was an issue geting video "' + videoId + '"');
+        }
+        
     };
 });
