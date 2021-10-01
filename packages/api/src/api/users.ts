ac3a2fd89e0cb4c72d16225fbb58b7eedfa62e41
Kevin <25735164+NotKevin@users.noreply.github.com>
2021-10-01 00:03:30 -0500

    add get api to display all users (not needed)

--- packages/api/src/api/users.ts
+++ packages/api/src/api/users.ts
@@ -7,8 +7,10 @@ export const users = Router();
 
 users.get('', async (_req, res) => {
 
+  const userRepository = req.entityManager.getRepository(User);
+  const selectedUser = await userRepository.findAll(); 
 
-    res.send({});
+    res.sendStatus(200).send({selectedUser});
   });
 
 users.get('/:userId', async (req, res) => {
@@ -16,9 +18,7 @@ users.get('/:userId', async (req, res) => {
   const userRepository = req.entityManager.getRepository(User);
   const selectedUser = await userRepository.findOne({ id : req.params.userId }); 
 
-  res.send({
-    selectedUser
-  });
+  res.sendStatus(200).send({selectedUser});
 });
 
 // add GET endpoint to return all user profile information from database
