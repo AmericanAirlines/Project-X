dbcc37c12c3c251a5e45d4b4ac2ba01fdba377be
Ayush Agarwal <ayush0316@gmail.com>
2021-10-03 21:49:03 -0500

    Update User.ts
    - make pronouns, schoolName, major, graduationDate optional
    - make hireable required
    - remove optional fields from constructor

--- packages/api/src/entities/User.ts
+++ packages/api/src/entities/User.ts
@@ -10,38 +10,35 @@ export class User extends Node<User> {
   @Property({ columnType: 'text' })
   name: string;
 
-  @Property({ columnType: 'text' })
-  pronouns: string;
+  @Property({ columnType: 'text', nullable: true })
+  pronouns?: string;
 
-  @Property({ columnType: 'text' })
+  @Property({ columnType: 'text', nullable: true })
   location?: string;
-
-  @Property( { columnType: 'boolean'})
-  hireable?: boolean;
-
+  
+  @Property( { columnType: 'boolean' })
+  hireable: boolean;
+  
   @Property({ columnType: 'text' })
   purpose: string;
-
-  @Property({ columnType: 'text' })
-  schoolName:string;
-
-  @Property({ columnType: 'text' })
-  major: string;
-
-  @Property({ columnType: 'Date' })
+  
+  @Property({ columnType: 'text', nullable: true })
+  schoolName?: string;
+  
+  @Property({ columnType: 'text', nullable: true })
+  major?: string;
+  
+  @Property({ columnType: 'Date', nullable: true })
   graduationDate?: Date;
 
 
-  constructor({ name, pronouns, location, hireable, purpose, schoolName, major, graduationDate, ...extraValues }: UserConstructorValues) {
+  constructor({ name, hireable, purpose, ...extraValues }: UserConstructorValues) {
     super(extraValues);
 
     this.name = name;
-   
-    this.pronouns = pronouns;
+    this.hireable = hireable;
     this.purpose = purpose;
-    this.schoolName = schoolName;
     this.major = major;
-    this.graduationDate = graduationDate;
-    // add other fields to constructor
+
   }
 }
