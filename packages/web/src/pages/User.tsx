a679264f82ca8b4efbd320d18a039816c34b23e9
NotKevin <25735164+NotKevin@users.noreply.github.com>
2021-10-06 02:05:45 -0500

    Create User profile page and pseudocode

---
+++ packages/web/src/pages/User.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NextPage } from 'next';
+import { Heading } from '@chakra-ui/layout';
+import { MarketingLayout } from '../components/Layout';
+
+const User: NextPage = () => {
+    // Make page stateful
+    // Fetch user data using users API
+    // Display fetched user data in their respective fields
+
+    // Things to think about:
+    // What to return if invalid user id is passed into API (or what happens if API fails to retrieve any data)
+    // Figure out the layout of the page
+
+    return (
+        <MarketingLayout>
+            <Heading>Profile Page</Heading>
+        </MarketingLayout>
+    );
+};
+
+export default User;
