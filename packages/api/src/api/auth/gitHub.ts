e1932d45a647755ad655c7c71cf5d9880ae247fa
Tuan Desktop <leetuan1998@yahoo.com>
2021-10-05 22:54:07 -0500

    testing with passportjs

--- packages/api/src/api/auth/gitHub.ts
+++ packages/api/src/api/auth/gitHub.ts
@@ -2,16 +2,24 @@ import { Router } from 'express';
 
 import { env } from '../../env';
 
+const passport = require('passport');
+
 const GitHubClientId = env.GitHubId;
 
 export const gitHub = Router();
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 gitHub.get('/github/login', (req, res) => {
+  /* passport(
+    `https://github.com/login/oauth/authorize?client_id=${GitHubClientId}&redirect_uri=http://localhost:3000/api/auth/github/callback`,
+  ); */
+
   const url = `https://github.com/login/oauth/authorize?client_id=${GitHubClientId}&redirect_uri=http://localhost:3000/api/auth/github/callback`;
   res.redirect(url);
 });
 
 gitHub.get('/github/callback', (req, res) => {
+  // passport.authenticate('github', { failureRedirect: '/login' })
   // const url = ``;
   res.redirect(``);
 });
