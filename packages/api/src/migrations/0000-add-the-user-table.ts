e9107f07d6f6e31523e8c70f3bfb8f63a3759393
NotKevin <25735164+NotKevin@users.noreply.github.com>
2021-10-07 01:29:00 -0500

    Revert 0000 migration to original version

--- packages/api/src/migrations/0000-add-the-user-table.ts
+++ packages/api/src/migrations/0000-add-the-user-table.ts
@@ -3,7 +3,7 @@ import { Migration } from '@mikro-orm/migrations';
 export class Migration20211003200607 extends Migration {
 
   async up(): Promise<void> {
-    this.addSql('create table "user" ("id" bigserial primary key, "createdAt" timestamptz(0) not null default clock_timestamp(), "updatedAt" timestamptz(0) not null default clock_timestamp(), "name" text not null, "pronouns" text null, "location" text not null, "hireable" boolean not null, "purpose" text null, "schoolName" text not null, "major" text not null, "graduationDate" Date not null);');
+    this.addSql('create table "user" ("id" bigserial primary key, "createdAt" timestamptz(0) not null default clock_timestamp(), "updatedAt" timestamptz(0) not null default clock_timestamp(), "name" text not null);');
   }
 
   async down(): Promise<void> {
