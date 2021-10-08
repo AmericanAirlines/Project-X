7f5caf4870219533fc87310d8dd5ba62d3ec2e73
NotKevin <25735164+NotKevin@users.noreply.github.com>
2021-10-08 07:50:46 -0500

    Add userprofile.tsx and UserProfileLayout.tsx

---
+++ packages/web/src/components/Layout/UserProfileLayout.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Heading, HStack, Spacer, useTheme, VStack, Text, Box } from '@chakra-ui/react';
+
+export interface UserProfileData {
+  name: string;
+  pronouns?: string;
+  location?: string;
+  hireable: boolean;
+  purpose: string;
+  schoolName?: string;
+  major?: string;
+  graduationDate?: Date;
+}
+
+export const UserProfileLayout: React.FC<UserProfileData> = ( user: UserProfileData ) => {
+    const theme = useTheme();
+  
+    // Currently only showing the "non-sensitive fields" (see users API)
+    return (
+      <Box border="1px" borderColor="gray.200" boxShadow="base" p={3}>
+        <Text fontSize="6xl">{user.name}</Text>
+        <Text fontSize="xl">{user.pronouns}</Text>
+        <Text fontSize="xl">{user.schoolName}</Text>
+      </Box>
+    );
+  };
+  
--- packages/web/src/pages/userprofile.tsx
+++ packages/web/src/pages/userprofile.tsx
@@ -1,22 +1,73 @@
 import React from 'react';
 import { NextPage } from 'next';
-import { Heading } from '@chakra-ui/layout';
 import { MarketingLayout } from '../components/Layout';
+import queryString from 'query-string';
+import { UserProfileLayout, UserProfileData } from '../components/Layout/UserProfileLayout';
+import { Alert, AlertIcon } from '@chakra-ui/alert';
 
 const UserProfile: NextPage = () => {
-    // Make page stateful
-    // Fetch user data using users API
-    // Display fetched user data in their respective fields
+    const [user, setUser] = React.useState<UserProfileData>({name: "", hireable: false, purpose: ""});
+    const [isValidUser, setIsValidUser] = React.useState<boolean>(false); // Flag used to catch whether id entered is in database
 
-    // Things to think about:
-    // What to return if invalid user id is passed into API (or what happens if API fails to retrieve any data)
-    // Figure out the layout of the page
+    React.useEffect(() => {
+        const fetchStatus = async () => {
 
-    return (
-        <MarketingLayout>
-            <Heading>Profile Page</Heading>
-        </MarketingLayout>
-    );
+            // Assuming that you need a query parameter to see a specific user's profile page (/userprofile?id=#)
+            // Concern?: Goes this route when there are additional parameters after id (/userprofile?id=#&key=value)
+            if(!isNaN(Number(queryString.parse(location.search).id)))
+            {
+                const userAPIQuery: string = "/api/users/" + queryString.parse(location.search).id;
+                
+                // If user id doesn't exist in database, throw the 400 error from users API
+                // Concern?: Catch block has nothing since isValidUser is by default set to false
+                try
+                {
+                    const res = await fetch(userAPIQuery);
+                    const data = await res.json();
+
+                    setUser(data);
+
+                    setIsValidUser(true);
+                }
+                catch
+                {
+
+                }
+
+            }
+             // When login is implemented, should get user id from session and return logged in user's data (if logged in)
+             // Concern?: ^ is this the idea?
+            else
+            {
+               // setUser(null);
+            }
+            
+        };
+
+        fetchStatus();
+    }, []);
+
+    // When user id not entered as query parameter or could not be found in the database
+    // Concern?: The user's name is required value in the database, default user state assigns "" as name. Don't know if this is ok to find bad input
+    if(user.name === "")
+    {
+        return (
+            <MarketingLayout>
+                <Alert status="error">
+                    <AlertIcon/>
+                    User could not be found
+                </Alert>
+            </MarketingLayout>
+        );
+    }
+    else // On valid and found user id
+    {
+        return (
+            <MarketingLayout>
+                <UserProfileLayout {...user} />
+            </MarketingLayout>
+        );
+    }
 };
 
 export default UserProfile;
