ba9d8327a30c76a7cc700133a38f66a99017621a
Ayush Agarwal <ayush0316@gmail.com>
2021-10-08 02:10:47 -0500

    Update users.ts
    add input validation, error handling, clean up implementation

--- packages/api/src/api/users.ts
+++ packages/api/src/api/users.ts
@@ -1,20 +1,39 @@
 import { Router } from 'express';
 import { User } from '../entities/User';
+import logger from '../logger';
 
 export const users = Router();
 
-users.get('', async (_req, res) => {
-
-  const userRepository = _req.entityManager.getRepository(User);
-  const selectedUser = await userRepository.findAll(); 
-
-  res.send({selectedUser});
+// const publicFields: (keyof typeof User)[] = ['pronouns', 'wat'];
+const stripSensitiveFields = (user: User): Partial<User> => ({
+  name: user.name,
+  pronouns: user.pronouns,
+  schoolName: user.schoolName,
 });
 
 users.get('/:userId', async (req, res) => {
+  const { userId } = req.params;
 
-  const userRepository = req.entityManager.getRepository(User);
-  const selectedUser = await userRepository.findOne({ id : req.params.userId }); 
+  try {
+    // Check if userId is in correct format
+    if (Number.isNaN(userId)) {
+      res.status(400).send(`"${userId}" is not a valid id, it must be a number.`);
+      return;
+    }
 
-  res.send({selectedUser});
+    // Find specified user within table
+    const user = await req.entityManager.findOne(User, { id: req.params.userId });
+
+    // Check if user exists
+    if (!user) {
+      res.sendStatus(404);
+      return;
+    }
+
+    // Return stripped user information
+    res.send(stripSensitiveFields(user));
+  } catch (error) {
+    logger.error(`There was an issue geting video "${userId}"`, error);
+    res.status(500).send(`There was an issue geting user "${userId}"`);
+  }
 });
