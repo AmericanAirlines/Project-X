2890c6b69dbe10df87b4141538d27b941564a1b1
Ayush Agarwal <ayush0316@gmail.com>
2021-10-15 03:12:25 -0500

    implement patch endpoint

--- packages/api/src/api/users.ts
+++ packages/api/src/api/users.ts
@@ -32,21 +32,71 @@ users.get('/:userId', async (req, res) => {
 
     // Return stripped user information
     res.status(200).send(stripSensitiveFields(user));
-  }
-  catch (error) {
+  } catch (error) {
     logger.error(`There was an issue getting user "${userId}"`, error);
     res.status(500).send(`There was an issue geting user "${userId}"`);
   }
 });
 
- users.patch('/:userId', async (req, res) => {
-  // const { userId } = req.params;
-  
-  // Also need list of User object columns to update - part of url?
-  
-  // Check if user exists
-  // Set edited columns in DB to specified values
-  // Use QueryBuilder to make a Patch query to DB
-  // https://mikro-orm.io/docs/query-builder/
+// Current assumptions:
+// Body data is in JSON format
+// Questions:
+// What fields can't be updated?
+users.patch('/:userId', async (req, res) => {
+  const { userId } = req.params;
 
-})
+  try {
+    if (Number.isNaN(Number(userId))) {
+      res.status(400).send(`"${userId}" is not a valid id, it must be a number.`);
+      return;
+    }
+
+    const user = await req.entityManager.findOne(User, { id: req.params.userId });
+
+    if (!user) {
+      res.sendStatus(404);
+      return;
+    }
+
+    if (req.body.name !== undefined) {
+      user.name = req.body.name;
+    }
+
+    if (req.body.pronouns !== undefined) {
+      user.pronouns = req.body.pronouns;
+    }
+
+    if (req.body.location !== undefined) {
+      user.location = req.body.location;
+    }
+
+    if (req.body.hireable !== undefined) {
+      user.hireable = req.body.hireable;
+    }
+
+    if (req.body.purpose !== undefined) {
+      user.purpose = req.body.purpose;
+    }
+
+    if (req.body.schoolName !== undefined) {
+      user.schoolName = req.body.schoolName;
+    }
+
+    if (req.body.major !== undefined) {
+      user.major = req.body.major;
+    }
+
+    // Concern: The Date constructor lets you put in just the month number and will default the Date to (month)-1-2001
+    // Do we force a date format? (MM-DD-YYYY, MM/DD/YYYY, etc.)
+    if (req.body.graduationDate !== undefined) {
+      user.graduationDate = new Date(req.body.graduationDate);
+    }
+
+    await req.entityManager.flush();
+
+    res.status(200).send(user);
+  } catch (error) {
+    logger.error(`There was an issue updating user "${userId}"`, error);
+    res.status(500).send(`There was an issue updating user "${userId}"`);
+  }
+});
