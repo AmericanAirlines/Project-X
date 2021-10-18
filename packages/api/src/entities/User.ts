8f87b1bf3b011a313c1d08f90d2154ac4ebffec2
Ayush Agarwal <ayush0316@gmail.com>
2021-10-18 10:24:33 -0500

    Create User profile API Endpoint (#27)
    * initial user profile api endpoint
    
    update index.ts with api route
    create user.ts with GET endpoint and use User entity
    
    * implement get user api
    
    - rename api route from /user to /users
    - modified User entity to contain new user information fields
    
    * adding newlines
    
    * Update User.ts
    
    - make pronouns, schoolName, major, graduationDate optional
    - make hireable required
    - remove optional fields from constructor
    
    * Update User.ts
    
    * generate migration for update user entity
    
    * Update users.ts
    
    add input validation, error handling, clean up implementation
    
    * Update users.ts
    
    add status 200 for good response
    
    * fix spelling
    
    * initial testing
    
    * Update users.ts
    
    * rename user table migration file
    
    * ran prettier

--- packages/api/src/entities/User.ts
+++ packages/api/src/entities/User.ts
@@ -10,9 +10,32 @@ export class User extends Node<User> {
   @Property({ columnType: 'text' })
   name: string;
 
-  constructor({ name, ...extraValues }: UserConstructorValues) {
+  @Property({ columnType: 'text', nullable: true })
+  pronouns?: string;
+
+  @Property({ columnType: 'text', nullable: true })
+  location?: string;
+
+  @Property({ columnType: 'boolean' })
+  hireable: boolean;
+
+  @Property({ columnType: 'text' })
+  purpose: string;
+
+  @Property({ columnType: 'text', nullable: true })
+  schoolName?: string;
+
+  @Property({ columnType: 'text', nullable: true })
+  major?: string;
+
+  @Property({ columnType: 'Date', nullable: true })
+  graduationDate?: Date;
+
+  constructor({ name, hireable, purpose, ...extraValues }: UserConstructorValues) {
     super(extraValues);
 
     this.name = name;
+    this.hireable = hireable;
+    this.purpose = purpose;
   }
 }
