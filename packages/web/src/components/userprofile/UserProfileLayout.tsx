3bf31dfaa3d1e0ad8af06ddfb71a2b066ae85d7e
Jason <25735164+NotKevin@users.noreply.github.com>
2021-10-18 11:47:51 -0500

    Update packages/web/src/components/userprofile/UserProfileLayout.tsx
    Co-authored-by: John Kahn <7807353+johnkahn@users.noreply.github.com>

--- packages/web/src/components/userprofile/UserProfileLayout.tsx
+++ packages/web/src/components/userprofile/UserProfileLayout.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import { Heading, HStack, Spacer, useTheme, VStack, Text, Box } from '@chakra-ui/react';
 
-export interface UserProfileData {
-  name: string;
-  pronouns?: string;
-  location?: string;
-  hireable: boolean;
-  purpose: string;
-  schoolName?: string;
-  major?: string;
-  graduationDate?: Date;
+export interface UserProfileProps {
+  user: {
+    name: string;
+    pronouns?: string;
+    schoolName?: string;
+  }
 }
 
-export const UserProfileLayout: React.FC<UserProfileData> = (user: UserProfileData) => {
-  const theme = useTheme();
-
-  // Currently only showing the "non-sensitive fields" (see users API)
+export const UserProfile: React.FC<UserProfileProps> = (user: UserProfileData) => {
   return (
     <Box border="1px" borderColor="gray.200" boxShadow="base" p={3}>
       <Text fontSize="6xl">{user.name}</Text>
