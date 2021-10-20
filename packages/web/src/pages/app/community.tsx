436f8ca33cb9edae11d6e931f3b265b9c4e14d8f
Tevin Beckwith <txb131330@utdallas.edu>
2021-10-20 00:25:46 -0500

    Community Page Iitial setup

---
+++ packages/web/src/pages/app/community.tsx
@@ -0,0 +1,76 @@
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Button, Divider, Heading, HStack, Link, Spacer, Text, useColorModeValue } from '@chakra-ui/react';
+import { NextPage } from 'next';
+import React from 'react';
+import { FaDiscord } from 'react-icons/fa';
+import { AppLayout } from '../../components/Layout';
+
+// To Do:
+// - move styling to /theme/index
+// - move discord link to env variables
+
+const Community: NextPage = () => {
+  return (
+    <AppLayout>
+      <Heading>Community</Heading>
+      <Accordion defaultIndex={[0]} allowMultiple allowToggle>
+        <AccordionItem>
+            <AccordionButton>
+                <Box flex="1" textAlign="left">
+                Community Guidelines
+                </Box>
+                <AccordionIcon />
+            </AccordionButton>
+
+            <AccordionPanel pb={4}>
+                <Text>1.  Be respectful.</Text>
+                <Text>2.  Be professional.</Text>
+                <Text>3.  Do not let others use your account.</Text>
+                <Text>4.  Ask questions!</Text>         
+            </AccordionPanel>
+        </AccordionItem>
+
+        <AccordionItem>
+            <AccordionButton>
+                <Box flex="1" textAlign="left">
+                Joining the Discord Server
+                </Box>
+                <AccordionIcon />
+            </AccordionButton>
+
+            <AccordionPanel pb={4}>
+            <Text pb={4}>Anyone can join the Project-X Discord server, but only user with linked Discord accounts will be able to actively participate.</Text>
+            <Text pb={4}>You can do this by naviagting to your profile and clicking the &quot;Link Discord&quot; button or by clicking <Link href="/api/auth/discord/login" textColor="blue">here.</Link></Text>
+            <Text pb={4}>Remeber to follow the Community Guidlines listed above.</Text>
+            </AccordionPanel>
+        </AccordionItem>
+        <AccordionItem>
+            <AccordionButton>
+                <Box flex="1" textAlign="left">
+                FAQ
+                </Box>
+                <AccordionIcon />
+            </AccordionButton>
+            <AccordionPanel pb={4}>
+            <Heading as="h3" size="sm">What if I need to change my linked Discord account?</Heading>
+            <Text pb={4}>This is not currently possible, but may be soon!</Text>
+    
+            <Heading as="h3" size="sm">Should I change my Discord profile avatar?</Heading>
+            <Text>If your profile avatar is something offensive then you should change it immediately.  If you are unsure then you should probably change it.</Text>
+            </AccordionPanel>
+        </AccordionItem>
+        </Accordion>
+        <HStack>
+            <Spacer />
+            <Button bg="#7289da" textColor="#ffffff" leftIcon={<FaDiscord color="#ffffff" />}>
+                <Link href="https://discord.gg/pMhrYKbVGS">
+                    Join our Discord
+                </Link>            
+            </Button>
+            <Spacer />
+        </HStack>
+        
+    </AppLayout>
+  );
+};
+
+export default Community;
---
+++ packages/web/tests/pages/app/community.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { render, screen, waitFor } from '../../testUtils/testTools';
+import { getMock } from '../../testUtils/getMock';
+import { AppLayout } from '../../../src/components/Layout';
+import Community from '../../../src/pages/app/community';
+
+jest.mock('../../../src/components/Layout/AppLayout.tsx');
+getMock(AppLayout).mockImplementation(({ children }) => <>{children}</>);
+
+
+describe('community page', () => {
+  beforeEach(async () => {
+    jest.clearAllMocks();
+  });
+
+  it('renders', async () => {
+    expect(() => render(<Community />)).not.toThrow();
+
+    expect(screen.getByText('Community')).toBeVisible();
+
+    // Check that the Community Guidelines section is displayed by default
+    await waitFor(() => expect(screen.getByText('1. Be respectful.')).toBeVisible());
+
+    expect(screen.queryByText('Remeber to follow the Community Guidlines listed above.')).not.toBeVisible();
+    expect(screen.queryByText('Should I change my Discord profile avatar?')).not.toBeVisible();
+    expect(screen.queryByText('Join our Discord')).toBeVisible();
+    //expect(screen.queryByRole('link')).toHaveAttribute('href', '/test/discord/link');
+  });
+
+
+});
