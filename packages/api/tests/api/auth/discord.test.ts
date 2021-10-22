3c86ac7b1fd9519727b584d6abfb01522e5548d4
Tevin Beckwith <txb131330@utdallas.edu>
2021-10-22 05:23:29 -0500

    added tests

--- packages/api/tests/api/auth/discord.test.ts
+++ packages/api/tests/api/auth/discord.test.ts
@@ -16,8 +16,8 @@ const testResponse: DiscordAccessTokenResponse = {
 const testDiscordUser = {
   id: '12344',
 };
-let axiosGetSpy = jest.spyOn(axios, 'get').mockResolvedValueOnce({ data: testDiscordUser });
-let axiosPostSpy = jest.spyOn(axios, 'post').mockResolvedValueOnce({ data: testResponse });
+const axiosGetSpy = jest.spyOn(axios, 'get').mockResolvedValueOnce({ data: testDiscordUser });
+const axiosPostSpy = jest.spyOn(axios, 'post').mockResolvedValueOnce({ data: testResponse });
 const loggerSpy = jest.spyOn(logger, 'error').mockImplementation();
 
 const url = `https://discord.com/api/oauth2/authorize?client_id=${env.discordClientId}&redirect_uri=http://localhost:3000/api/auth/discord/callback&response_type=code&scope=identify`;
