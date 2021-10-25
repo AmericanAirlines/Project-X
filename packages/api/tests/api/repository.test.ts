c4e265309d9957d72fbaca91aa3d6a41d7c2e6a0
NotKevin <25735164+NotKevin@users.noreply.github.com>
2021-10-25 12:44:46 -0500

    updated tests (still incomplete)

--- packages/api/tests/api/repository.test.ts
+++ packages/api/tests/api/repository.test.ts
@@ -24,27 +24,27 @@ const MockRepositoryResults: MockRepositoryDetails[] = [
     {url: "www.github.com/AmericanAirlines/EvenBiggerPlane", name: "EvenBiggerPlane", stargazer_count: 54321}
 ];
 
-// const MockBody = {
-//     query: `
-//             query ($id: [ID!]!){
-//               nodes(ids: $id){
-//                 ...on Repository{
-//                   name
-//                   url
-//                   stargazerCount
-//                 }
-//               }
-//             }
-//             `,
-//     variables: {
-//         id: MockRepositoryList,
-//     }
-// }
+const MockBody = {
+    query: `
+            query ($id: [ID!]!){
+              nodes(ids: $id){
+                ...on Repository{
+                  name
+                  url
+                  stargazerCount
+                }
+              }
+            }
+            `,
+    variables: {
+        id: MockRepositoryList,
+    }
+}
 
 describe("Repository GET route", () => {
     beforeEach(async() => {
         jest.clearAllMocks();
-        fetchMock.mockClear();
+        fetchMock.reset();
     })
 
     it("error on find returns 500 status code", async () => {
@@ -60,6 +60,7 @@ describe("Repository GET route", () => {
         const handler = testHandler(repository);
         handler.entityManager.find.mockResolvedValueOnce(MockRepositoryList);
         
+        // vvv doesn't work (returns 500 Internal Service Error for line 91)
         // fetchMock.post(
         //     {
         //         url:"https://api.github.com/graphql",
@@ -77,22 +78,18 @@ describe("Repository GET route", () => {
         //         }
         //     });
 
-
-
         // const fetchRes = await fetch("https://api.github.com/graphql", {method: "POST", headers:{ 'Content-Type': 'application/json', 'Authorization': 'bearer RealToken'}, body: JSON.stringify(MockBody)});
 
-        fetchMock.post("https://api.github.com/graphql", MockRepositoryResults);
-        await fetch("https://api.github.com/graphql", {method: "POST"});
-
-        // Ignore these 4 tests
+        // // attempt at tests for ^
         // expect(fetchRes.status).toEqual(200); // how to check status code
         // expect(await fetchRes.json()).toEqual({data: MockRepositoryResults}); // how to check response body
-        // expect(fetchMock).toHaveBeenCalledWith("https://api.github.com/graphql", {body: JSON.stringify(MockBody), headers:{ 'Content-Type': 'application/json',  'Authorization': 'bearer RealToken'}, method: "POST"}); // How to check args of fetch
         // expect(fetchMock).toHaveLastFetched("https://api.github.com/graphql"); // How to check what link you're fetching from
-        
 
-        const { body } = await handler.get('/').expect(200); // <-- Returns "500 Internal Server Error" using commented out fetchmock
+        fetchMock.post("https://api.github.com/graphql", MockRepositoryResults); // <- works
+
+        const { body } = await handler.get('/').expect(200);
         expect(body).toEqual(MockRepositoryResults);
-        expect(fetchMock).toHaveBeenCalledWith("https://api.github.com/graphql", {method: "POST"});
+        expect(fetchMock).toHaveLastFetched("https://api.github.com/graphql", 'post');
+        expect(fetchMock).toHaveFetchedTimes(1);        
     });
 });
