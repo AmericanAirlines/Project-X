c4aef3ea656d2013f21001235ea127d9e367b9e2
Ayush Agarwal <ayush0316@gmail.com>
2021-10-25 02:07:07 -0500

    ran prettier

--- packages/api/tests/testUtils/testHandler.ts
+++ packages/api/tests/testUtils/testHandler.ts
@@ -7,7 +7,9 @@ import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 
 type TestRequestHandler = RequestHandler<ParamsDictionary, any, any, ParsedQs, Record<string, any>>;
 
-type MockEntityManager = jest.Mocked<Pick<EntityManager<PostgreSqlDriver>, 'find' | 'findOne'>>;
+type MockEntityManager = jest.Mocked<
+  Pick<EntityManager<PostgreSqlDriver>, 'find' | 'findOne' | 'flush'>
+>;
 
 type SuperTestWithEntityManager = SuperTest<Test> & { entityManager: MockEntityManager };
 
@@ -16,6 +18,7 @@ const createTestApp = (handler: TestRequestHandler) => {
   const entityManager: MockEntityManager = {
     find: jest.fn(),
     findOne: jest.fn(),
+    flush: jest.fn(),
   };
 
   const app = express();
