2c53ae429e9396322fe257292ac0de6fd10ce512
Tuan Desktop <leetuan1998@yahoo.com>
2021-10-29 18:06:05 -0500

    navbar undo change and yarn prettyfix

--- packages/web/src/components/NavBar/NavBar.tsx
+++ packages/web/src/components/NavBar/NavBar.tsx
@@ -5,7 +5,7 @@ import { NavProfileMenu } from './NavProfileMenu';
 
 export const NavBar: React.FC = () => {
   return (
-    <HStack >
+    <HStack bg={useColorModeValue('gray.100', 'gray.900')}>
       <NavLink label="Home" href="/app" />
       <NavLink label="Community" href="/app/community" />
       <NavLink label="Videos" href="/app/videos" />
