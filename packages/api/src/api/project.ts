ece50dd78c1cfa1c465a77170ebb8dee47d39cb0
Jason <25735164+NotKevin@users.noreply.github.com>
2021-11-01 15:37:48 -0500

    Project Entity and GET route (#49)
    * Repository Entity and GET route boilerplate
    
    * wip
    
    * Finish get route and create entity / migration
    
    * Update packages/api/src/api/repository.ts
    
    Co-authored-by: Demondre Livingston <livingstondemondre7@gmail.com>
    
    * Update packages/api/src/api/repository.ts
    
    Co-authored-by: Demondre Livingston <livingstondemondre7@gmail.com>
    
    * Update packages/api/src/api/repository.ts
    
    Co-authored-by: Demondre Livingston <livingstondemondre7@gmail.com>
    
    * add tests (incomplete?)
    
    * updated tests (still incomplete)
    
    * from meeting
    
    * rename repository -> project
    
    * Change to use user github token
    
    * Update packages/api/src/api/project.ts
    
    Co-authored-by: John Kahn <7807353+johnkahn@users.noreply.github.com>
    
    * Update packages/api/src/api/project.ts
    
    Co-authored-by: John Kahn <7807353+johnkahn@users.noreply.github.com>
    
    * Update packages/api/src/index.ts
    
    Co-authored-by: John Kahn <7807353+johnkahn@users.noreply.github.com>
    
    * Update packages/api/src/index.ts
    
    Co-authored-by: John Kahn <7807353+johnkahn@users.noreply.github.com>
    
    * Update packages/api/src/index.ts
    
    Co-authored-by: John Kahn <7807353+johnkahn@users.noreply.github.com>
    
    * Update packages/api/tests/api/project.test.ts
    
    Co-authored-by: John Kahn <7807353+johnkahn@users.noreply.github.com>
    
    * Add checks for user session, finished tests
    
    * yarn prettier and lint
    
    * - update users patch api to work with new session data object,
    - update users api test for user authorization
    - update yarn lock file
    
    * yarn prettier and lint
    
    * - As suggested in PR #50, fixed req.user so id is in profile
    - Made corresponding changes to user and project route and tests
    
    * yarn prettier and lint. Remove duplicate in package.json
    
    * Update packages/api/src/api/users.ts
    
    Co-authored-by: John Kahn <7807353+johnkahn@users.noreply.github.com>
    
    * update formatting for else block
    
    * yarn prettier and lint
    
    Co-authored-by: NotKevin <25735164+NotKevin@users.noreply.github.com>
    Co-authored-by: Demondre Livingston <livingstondemondre7@gmail.com>
    Co-authored-by: John Kahn <7807353+johnkahn@users.noreply.github.com>

--- packages/api/src/api/project.ts
+++ packages/api/src/api/project.ts
@@ -49,5 +49,49 @@ project.post('', async (req, res) => {
       logger.error(errorMessage, error);
       res.status(500).send(errorMessage);
     }
-  } else res.sendStatus(401);
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+project.get('', async (req, res) => {
+  if (req.user) {
+    try {
+      const projectList = await req.entityManager.find(Project, {});
+      const repoNodeIds = projectList.map((repo) => repo.nodeID);
+
+      const fetchRes = await fetch('https://api.github.com/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `bearer ${req.user.githubToken}`,
+        },
+        body: JSON.stringify({
+          query: `
+            query ($id: [ID!]!){
+              nodes(ids: $id){
+                ...on Repository{
+                  name
+                  url
+                  stargazerCount
+                  description
+                }
+              }
+            }
+            `,
+          variables: {
+            id: repoNodeIds,
+          },
+        }),
+      });
+
+      res.status(fetchRes.status).send(await fetchRes.json());
+    } catch (error) {
+      const errorMessage = 'There was an issue getting the projects';
+      logger.error(errorMessage, error);
+      res.status(500).send(errorMessage);
+    }
+  } else {
+    res.sendStatus(401);
+  }
 });
