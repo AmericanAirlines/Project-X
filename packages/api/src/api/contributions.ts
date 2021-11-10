f4d3f61e4014dc10b7ca5e2548ce335f23d4a35f
NotKevin <25735164+NotKevin@users.noreply.github.com>
2021-11-10 09:58:55 -0600

    fix wording on planning

--- packages/api/src/api/contributions.ts
+++ packages/api/src/api/contributions.ts
@@ -9,7 +9,11 @@ contributions.get('', async (req, res) => {
     {
         try {
             /*
-            * Plan: (fetch) search all contributions by the node id of all of the PRs in our DB, filter to only have the contributions with the current user's nodeID, and return the result
+            * Plan: 
+             - entityManager.find to get all entries in contribution table and store their node ids in an array
+             - query GraphQL for all contributions in our DB (use ^ array)
+             - store the node id's of the repos where the current user authored the PR in an array
+             - entityManager.find again for the ids in ^ array and return the result            
             */
 
     // GraphQL to see which of the contributions are by the currently logged in user:
@@ -40,7 +44,7 @@ contributions.get('', async (req, res) => {
         }
         catch (error)
         {
-            const errorMessage = 'There was in issue returning the contributions.';
+            const errorMessage = 'There was an issue returning the contributions.';
             logger.error(errorMessage, error);
             res.status(500).send(errorMessage);
         }
