6a82561924fd76fb9adb7c6d50aae719eebf965d
Tevin Beckwith <txb131330@utdallas.edu>
2021-11-14 09:55:26 -0600

    working data fetch and db save

--- packages/api/src/api/contributions.ts
+++ packages/api/src/api/contributions.ts
@@ -1,18 +1,37 @@
 import express, { Router } from 'express';
-import { env } from 'process';
+import { env } from '../env';
 import { Contribution } from '../entities/Contribution';
 import { User } from '../entities/User';
 import logger from '../logger';
+import { type } from 'os';
 
 export const contributions = Router();
 contributions.use(express.json());
+
+export interface PullRequestContribution {
+  id: string;
+  title: string;
+  permalink: string;
+  mergedAt: string;
+  author: {
+    login: string;
+  }
+}
+
+interface ContributionResponse {
+  data: {
+    search: {
+      nodes: PullRequestContribution[];
+    }
+  } 
+}
 
 
 contributions.get('', async (req, res) => {
   try {
       // Find a way to persist this instead of recalculating it
-      //const lastRun = Date.now() as unknown as Date;
-      //lastRun.setHours(lastRun.getDate() - 1);
+      // const lastRun = Date.now() as unknown as Date;
+      // lastRun.setHours(lastRun.getDate() - 1);
 
       // const usersList = await req.entityManager.find(User, {
       //   contributionsLastCheckedAt: {
@@ -21,7 +40,7 @@ contributions.get('', async (req, res) => {
       // });
 
       // Call GraphQL query for each user in userList at once
-        console.log(req.user)
+        
         const fetchRes = await fetch('https://api.github.com/graphql', {
           method: 'POST',
           headers: {
@@ -30,36 +49,43 @@ contributions.get('', async (req, res) => {
           },
           body: JSON.stringify({
             query: `
-            query { 
-              node(id: "MDEwOlJlcG9zaXRvcnk0MDQ0OTAyNDA=") {
-                ... on Repository {
-                  id
-                  name
-                  pullRequests (last: 100, states: [MERGED], ) {
-                    edges {
-                      node{
-                        ... on PullRequest {
-                          id
-                          title
-                          state
-                          closedAt
-                          author {
-                            login
-                          }
-                        }           
-                      }
+            {
+              search(first: 100, query: "repo:AmericanAirlines/Project-X repo:AmericanAirlines/simple-env is:pr is:merged merged:2020-01-01..2021-11-01 author:TevinBeckwith author:SpencerKeisen", type:ISSUE)
+              {
+                nodes{
+                  ... on PullRequest {
+                    id
+                    title
+                    permalink
+                    mergedAt
+                    author {
+                      login
                     }
-                  }
-                }   
-              }	
+                  }           
+                }
+              }
             }
                 `,
           }),
         });
   
-        const responseData = await fetchRes.json();
-    
-        res.send(responseData);
+        const {data: responseData} : ContributionResponse = await fetchRes.json();
+
+        for (const pr of responseData.search.nodes.entries()) {
+          const newContribution = new Contribution({
+            nodeID: pr[1].id,
+            type: 'Pull Request',
+            description: pr[1].title,
+            contributedAt: new Date(Date.parse(pr[1].mergedAt)),
+            score: 100,
+          })
+          
+          req.entityManager.persist(newContribution);
+        }
+
+        void req.entityManager.flush();
+
+        res.send('done');
         
       } catch (error) {
         const errorMessage = 'There was an issue saving contribution data to the database';
@@ -67,6 +93,4 @@ contributions.get('', async (req, res) => {
         res.status(500).send(errorMessage);
       }
       // Persist new info in the db
-
-      // use setTimeout to cause infinit loop of calling this endpoint every hour
   });
