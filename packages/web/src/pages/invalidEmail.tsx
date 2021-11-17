e446850b2824e113fba4e6812c95a6295340eea6
Nick Vitale <ntxnick@aol.com>
2021-11-17 14:09:12 -0600

    yarn prettier ran

--- packages/web/src/pages/invalidEmail.tsx
+++ packages/web/src/pages/invalidEmail.tsx
@@ -10,18 +10,18 @@ const InvalidEmail: NextPage = () => {
           Error: Make sure you have a verified &quot;.edu&quot; email linked with your GitHub
           account!
         </Heading>
-          <Text textAlign="center" maxWidth="60ch" fontSize="2xl" lineHeight="2">
-            Try again once you&apos;ve verified your student email address 👇
-          </Text>
-          <Button as="a" size="lg" colorScheme="gray" href="/api/auth/github/login">
-            Login with GitHub
-          </Button>
-          <Text textAlign="center" maxWidth="60ch" fontSize="2xl" lineHeight="2">
-            Click this button if you want to go back to the homepage 👇
-          </Text>
-          <Button as="a" size="lg" colorScheme="blue" href="/">
-            Home Page
-          </Button>
+        <Text textAlign="center" maxWidth="60ch" fontSize="2xl" lineHeight="2">
+          Try again once you&apos;ve verified your student email address 👇
+        </Text>
+        <Button as="a" size="lg" colorScheme="gray" href="/api/auth/github/login">
+          Login with GitHub
+        </Button>
+        <Text textAlign="center" maxWidth="60ch" fontSize="2xl" lineHeight="2">
+          Click this button if you want to go back to the homepage 👇
+        </Text>
+        <Button as="a" size="lg" colorScheme="blue" href="/">
+          Home Page
+        </Button>
       </VStack>
     </MarketingLayout>
   );
--- packages/web/tests/pages/InvalidEmail.test.tsx
+++ packages/web/tests/pages/InvalidEmail.test.tsx
@@ -11,15 +11,23 @@ describe('web /invalidEmail', () => {
   it('renders', async () => {
     expect(() => render(<InvalidEmail />)).not.toThrow();
 
-    expect(screen.queryByText('Error: Make sure you have a verified ".edu" email linked with your GitHub account!')).toBeVisible();
-    expect(screen.queryByText("Try again once you've verified your student email address 👇")).toBeVisible();
+    expect(
+      screen.queryByText(
+        'Error: Make sure you have a verified ".edu" email linked with your GitHub account!',
+      ),
+    ).toBeVisible();
+    expect(
+      screen.queryByText("Try again once you've verified your student email address 👇"),
+    ).toBeVisible();
     expect(screen.queryByText('Login with GitHub')).toBeVisible();
-    expect(screen.queryByText('Login with GitHub')).toHaveAttribute('href', '/api/auth/github/login');
-    expect(screen.queryByText('Click this button if you want to go back to the homepage 👇')).toBeVisible();
+    expect(screen.queryByText('Login with GitHub')).toHaveAttribute(
+      'href',
+      '/api/auth/github/login',
+    );
+    expect(
+      screen.queryByText('Click this button if you want to go back to the homepage 👇'),
+    ).toBeVisible();
     expect(screen.queryByText('Home Page')).toBeVisible();
     expect(screen.queryByText('Home Page')).toHaveAttribute('href', '/');
-
   });
-
-
 });
