625aa35f3cfd97ffe85533635d7e064bc74f997c
NotKevin <25735164+NotKevin@users.noreply.github.com>
2021-11-23 10:37:00 -0600

    change contributions get route from getting currently the info of the logged in user to getting the info a queried user

--- packages/api/src/api/contributions.ts
+++ packages/api/src/api/contributions.ts
@@ -1,58 +1,35 @@
 import { Router } from 'express';
 import { Contribution } from '../entities/Contribution';
+import { User } from '../entities/User';
 import logger from '../logger';
 
 export const contributions = Router();
 
 contributions.get('', async (req, res) => {
-  if (req.user) {
-    try {
-      const allContributionsList = await req.entityManager.find(Contribution, {});
-      const contributionNodeIds = allContributionsList.map((contribution) => contribution.nodeID);
+  if(!req.user) {
+    res.sendStatus(401);
+  }
 
-      const fetchAllContributionsRes = await fetch('https://api.github.com/graphql', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `bearer ${req.user.githubToken}`,
-        },
-        body: JSON.stringify({
-          query: `
-                query ($contributionArray: [ID!]!){
-                    nodes(ids: $contributionArray){
-                    ...on PullRequest{
-                        id
-                        viewerDidAuthor
-                    }
-                    }
-                }
-                `,
-          variables: {
-            contributionArray: contributionNodeIds,
-          },
-        }),
-      });
+  const { userId } = req.query;
+  if(!userId) {
+    res.status(400).send('No user id was given.');
+  }
 
-      const allContributions = await fetchAllContributionsRes.json();
+  if (Number.isNaN(Number(userId))) {
+    res.status(400).send(`"${userId}" is not a valid id, it must be a number.`);
+  }
 
-      const curUserContributionsJSON = allContributions.data.nodes.filter(
-        (contribution: { viewerDidAuthor: boolean }) => contribution.viewerDidAuthor === true,
-      );
-      const curUserContributionsNodeIds: string[] = curUserContributionsJSON.map(
-        (contribution: { id: string }) => contribution.id,
-      );
+  try {
+    const queriedUser = await req.entityManager.findOne(User, { id: userId as string });
 
-      const allCurUserContributionsList = await req.entityManager.find(Contribution, {
-        nodeID: curUserContributionsNodeIds,
-      });
+    const userContributions = await req.entityManager.find(Contribution, {authorGithubId: queriedUser?.githubId})
 
-      res.send(allCurUserContributionsList);
-    } catch (error) {
-      const errorMessage = 'There was an issue returning the contributions.';
-      logger.error(errorMessage, error);
-      res.status(500).send(errorMessage);
-    }
-  } else {
-    res.status(401).send('You must be logged in to view your contributions.');
+    res.send(userContributions);
+  }
+  catch (error)
+  {
+    const errorMessage = "An error has occurred retriving the user's contributions."
+    logger.error(errorMessage, error);
+    res.status(500).send(errorMessage);
   }
 });
