eb50255d43bb983399155c9634fec2a3a3de28b1
Nick Vitale <ntxnick@aol.com>
2021-12-03 11:54:52 -0600

    ran prettier and lint

--- packages/web/src/components/NavBar/NavProfileMenu.tsx
+++ packages/web/src/components/NavBar/NavProfileMenu.tsx
@@ -16,21 +16,15 @@ export const NavProfileMenu: React.FC = () => (
       <Avatar size={'sm'} />
     </MenuButton>
     <MenuList>
-    <Link variant="navbarMenuItem" href="/app/profile">
-      <MenuItem>
-          Edit Profile
-      </MenuItem>
+      <Link variant="navbarMenuItem" href="/app/profile">
+        <MenuItem>Edit Profile</MenuItem>
       </Link>
       <Link variant="navbarMenuItem" href="/app/contributions">
-      <MenuItem>
-          View Contributions
-      </MenuItem>
+        <MenuItem>View Contributions</MenuItem>
       </Link>
       <MenuDivider />
       <Link variant="navbarMenuItem" href="/api/auth/github/logout">
-      <MenuItem>
-          Log Out
-      </MenuItem>
+        <MenuItem>Log Out</MenuItem>
       </Link>
     </MenuList>
   </Menu>
